85656490b34c6c2106d05e999a1b355e2f9a61ee
frederikbraad <71885976+frederikbraad@users.noreply.github.com>
2022-06-03 09:43:47 +0200

    Create login-logo.php

---
+++ login-logo.php
@@ -0,0 +1,15 @@
+<?php
+
+function my_login_logo() { ?>
+    <style type="text/css">
+        #login h1 a, .login h1 a {
+            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/login-cmf3.png);
+		height:170px;
+		width:251px;
+		background-size: 251px 170px;
+		background-repeat: no-repeat;
+        	padding-bottom: 30px;
+        }
+    </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'my_login_logo' );
